704b65e469f660408c88650d9d0046ddc6deba4f
tanvirprince <tanvirprince69@gmail.com>
2021-09-26 02:40:42 +0600

    after slider ads

--- resources/views/home/inc/locations.blade.php
+++ resources/views/home/inc/locations.blade.php
@@ -46,7 +46,7 @@ if (isset($citiesOptions, $citiesOptions['hide_on_mobile']) and $citiesOptions['
 			<li> Advertise zone 1 </li>
 		</ul>
 		<a href="//{{ $image->ads_1_link }}" target="_blank">
-			<img class="lazyload img-thumbnail img-fluid no-margin" src="{{asset('/')}}assets/google_banner/ads_1/{{$image->ads_1 }}" style="max-height:150px;" alt="ads1">
+			<img class="lazyload img-thumbnail img-fluid no-margin" alt="{{ $image->ads_1_title }}" src="{{asset('/')}}assets/google_banner/ads_1/{{$image->ads_1 }}" style="max-height:150px;" alt="ads1">
 		</a>
 	</div>
 @else
